4c02255fcbc25a12e20c7c16a46e22b3a1ec7fe4
Jeremy Mattingly <jamattingly@live.com>
2020-03-01 19:19:04 -0600

    New functions in US_FullMap utilize state and effect in order to dynamically push user to new route depending upon what state they click, and passes through what government level they are searching for. Subsequently started using react-router-dom Switch to display an 'in progress' message if the routes are not contained in the Switch

--- src/components/maps/US_CountryMap/US_FullMap.js
+++ src/components/maps/US_CountryMap/US_FullMap.js
@@ -1,18 +1,37 @@
-import React from "react"
-import { Jumbotron, Container } from "reactstrap"
+import React, { useState, useEffect } from "react"
+import { Jumbotron } from "reactstrap"
 import US_CountryMapCard from "./US_CountryMapCard"
 import US_MapToolbar from "./US_MapToolbar"
 import "./US_FullMap.css"
+import convertRegion from "../../../modules/convertRegion"
 
-const US_FullMap = () => {
+const US_FullMap = (props) => {
+    const [ govLevel, changeGovLevel ] = useState('city')
+    const [ stateSelected, chooseStateSelect] = useState('')
+
+
+    const selectLevel = (event) => {
+        const levelValue = event.target.value
+        return changeGovLevel(levelValue)
+
+    }
+
+    const selectState = (event) => {
+        const clickedState = convertRegion.convertForRoute(event.target.id, 'toName')
+        return chooseStateSelect(clickedState)
+    }
+
+    useEffect(()=>{
+        if(stateSelected !== '') {
+            props.history.push(`/${stateSelected}`,govLevel)
+        }
+    },[stateSelected])
 
     return (
         <section className="fullMapContainer">
             <Jumbotron className="jumbo">
-                {/* <Container fluid> */}
-                    <US_MapToolbar />
-                    <US_CountryMapCard />
-                {/* </Container> */}
+                    <US_MapToolbar selectLevel={selectLevel}/>
+                    <US_CountryMapCard selectState={selectState}/>
             </Jumbotron>
         </section>
     )
